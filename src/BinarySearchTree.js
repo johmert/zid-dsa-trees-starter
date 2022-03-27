6bb68e14e9bb54f2070bcbeb00af9b54bf1ce5a5
johmert <johmert@users.noreply.github.com>
2022-03-26 21:40:05 -0700

    Depth-First search (pre and post order) completed

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -154,4 +154,43 @@ class BinarySearchTree {
     }
     return this.left._findMin();
   }
+  dfsInOrder(values = []) {
+    // First, process the left node recursively
+    if(this.left) {
+      values = this.left.dfsInOrder(values);
+    }
+    // Next, process the current node
+    values.push(this.value);
+    // Finally, process the right node recursively
+    if(this.right) {
+      values = this.right.dfsInOrder(values);
+    }
+    return values;
+  }
+  dfsPreOrder(values = []) {
+    // First, process the current node
+    values.push(this.value);
+    // Next, process the left node recursively
+    if(this.left) {
+      values = this.left.dfsPreOrder(values);
+    }
+    // Finally, process the right node recursively
+    if(this.right) {
+      values = this.right.dfsPreOrder(values);
+    }
+    return values;
+  }
+  dfsPostOrder(values = []) {
+    //First, process the left node recursively
+    if(this.left) {
+      values = this.left.dfsPostOrder(values);
+    }
+    // Next, process the right node recursively
+    if(this.right) {
+      values = this.right.dfsPostOrder(values);
+    }
+    // Finally, process the current node
+    values.push(this.value);
+    return values;
+  }
 }
